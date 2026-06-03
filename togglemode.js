83d78bf60ddabadb6541d599dc14366853706b88
Arthur Isensee <arthurisensee76@gmail.com>
2026-06-03 13:23:16 -0300

    feat: adicionei mudança de tema claro e dark

---
+++ togglemode.js
@@ -0,0 +1,28 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+function toggleTheme() {
+  document.body.classList.toggle("light");
+  document.body.classList.toggle("dark");
+
+  const elemento = document.querySelector('.main');
+
+  if (document.body.classList.contains("light")) {
+    const novaImagem = "images/Intro_light_mode.png";
+    themeToggle.innerHTML = "⏾";
+    themeToggle.style.backgroundColor = "black";
+    themeToggle.style.color = "white";
+    elemento.style.backgroundImage = `url('${novaImagem}')`;
+  } else {
+    themeToggle.innerHTML = "☀︎";
+    themeToggle.style.backgroundColor = "white";
+    themeToggle.style.color = "black";
+    // remove o inline style para voltar a imagem via CSS (Background_Intro.png)
+    elemento.style.backgroundImage = "";
+  }
+}
+
+// Adiciona evento de clique
+themeToggle.addEventListener("click", toggleTheme);
+
+// Define tema inicial (opcional)
+document.body.classList.add("dark");
